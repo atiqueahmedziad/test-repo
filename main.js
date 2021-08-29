0c07c96ab7a21795352850aa2d1f4deb4f5bd2eb
Atique Ahmed Ziad <softfilebd@gmail.com>
2021-08-30 03:30:57 +0600

    btn click events added

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const a=()=>{
-console.log('object')
-}
+const num1 = document.querySelector('#num1');
+const num2 = document.querySelector('#num2');
+const result = document.querySelector('#result');
+
+document.querySelector('#btn_plus').addEventListener('click', () => {
+  const res = add(parseInt(num1.value), parseInt(num2.value));
+  result.textContent = res;
+});
+
+document.querySelector('#btn_minus').addEventListener('click', () => {
+  const res = minus(parseInt(num1.value), parseInt(num2.value));
+  result.textContent = res;
+});
+
+document.querySelector('#btn_multiply').addEventListener('click', () => {
+  const res = multiply(parseInt(num1.value), parseInt(num2.value));
+  result.textContent = res;
+});
+
+document.querySelector('#btn_divide').addEventListener('click', () => {
+  const res = divide(parseInt(num1.value), parseInt(num2.value));
+  result.textContent = res;
+});
