856cefb8e35f373c4dcd0f9165ea6f85c001f4ec
saiffardin <saiffardin@gmail.com>
2021-08-30 17:55:07 +0600

    add-minus-mul-division functions added

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const num1 = document.querySelector('#num1');
 const num2 = document.querySelector('#num2');
 const result = document.querySelector('#result');
 
+const add = (n1,n2) => n1+n2;
+const minus = (n1,n2) => n1-n2;
+const multiply = (n1,n2) => n1*n2;
+const divide = (n1,n2) => n2==0 ? `Number 2 can't be 0` : n1/n2;
+
 document.querySelector('#btn_plus').addEventListener('click', () => {
   const res = add(parseInt(num1.value), parseInt(num2.value));
   result.textContent = res;
@@ -17,7 +22,7 @@ document.querySelector('#btn_multiply').addEventListener('click', () => {
   result.textContent = res;
 });
 
-document.querySelector('#btn_divide').addEventListener('click', () => {
+document.querySelector('#btn_division').addEventListener('click', () => {
   const res = divide(parseInt(num1.value), parseInt(num2.value));
   result.textContent = res;
 });
